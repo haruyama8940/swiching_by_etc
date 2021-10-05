82cc971442429bc4d3f87a51642d5be6eb431f16
haruyama8940 <yaruo0406@gmail.com>
2021-10-05 17:25:39 +0900

    set_gazebo_pose

--- src/pub_initpose.cpp
+++ src/pub_initpose.cpp
@@ -8,6 +8,7 @@
 #include <std_msgs/Bool.h>
 //#include <nav_msgs/Odometry.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Int8.h>
 #include <gazebo_msgs/LinkStates.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <gazebo_msgs/SetModelState.h>
@@ -26,8 +27,10 @@ ros::ServiceClient set_model_state_client;
 ros::ServiceServer srv;
 ros::Publisher initial_pose_pub; 
 ros::Subscriber reset_sub;
+ros::Publisher gazebo_pose_pub;
 
 void initial_pose_set(float pose_x,float pose_y,float ori_z,float ori_w)//initial_pose set function
+    
     {
     pose_msg.header.stamp = ros::Time::now();
     pose_msg.header.frame_id = "map";
@@ -38,13 +41,12 @@ void initial_pose_set(float pose_x,float pose_y,float ori_z,float ori_w)//initia
     pose_msg.pose.covariance[6 * 5 + 5] = 0.06853891945200942;
     pose_msg.pose.pose.orientation.z = ori_z;
     pose_msg.pose.pose.orientation.w = ori_w;
+    initial_pose_pub.publish(pose_msg);
     }
-void gazebo_pose_set(std::string model_name, 
-                    std::string reference_frame, 
-                    geometry_msgs::Pose pose, 
-                    geometry_msgs::Twist model_twist)
+    
+void gazebo_pose_set(std::string model_name, std::string reference_frame, geometry_msgs::Pose pose, geometry_msgs::Twist model_twist)
 
-{
+  {
   gazebo_msgs::SetModelState setmodelstate;
 
     // Model state msg
@@ -53,26 +55,16 @@ void gazebo_pose_set(std::string model_name,
     modelstate.reference_frame = reference_frame;
     modelstate.pose = pose;
     modelstate.twist = model_twist;
-    setmodelstate.request.model_state = modelstate;
+    gazebo_pose_pub.publish(modelstate);
 
-    // Call the service
-    bool success = set_model_state_client.call(setmodelstate);
-    if (success)
-    {
-        ROS_INFO_STREAM("Setting position of " << model_name << "model was successful.");
-    }
-    else
-    {
-        ROS_ERROR_STREAM("Setting position of " << model_name << "model was failed.");
-    }
-}
+  }
 
-void CallBack(const std_msgs::Bool& msg)
+void CallBack(const std_msgs::Int8 & msg)
  {
   geometry_msgs::Pose model_pose;
     model_pose.position.x = 0;
     model_pose.position.y = 0;
-    model_pose.position.z = 1;
+    model_pose.position.z = 0;
     model_pose.orientation.x = 0.0;
     model_pose.orientation.y = 0.0;
     model_pose.orientation.z = 0.0;
@@ -87,7 +79,7 @@ void CallBack(const std_msgs::Bool& msg)
     model_twist.angular.z = 0.0;
 
   initial_pose_set(-63.35,-105.17,0.96,0.279);
-  gazebo_pose_set("ball", "world", model_pose, model_twist);
+  gazebo_pose_set("mobile_base", "world", model_pose, model_twist);
 
  }
 
@@ -95,10 +87,16 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "pub_initpose");
   ros::NodeHandle nh;
-  ros::Publisher initial_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 10);
+  //ros::Publisher initial_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 10);
   ros::Rate loop_rate(10);
   //define 2d estimate pose
   initial_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 10);//initial_pose publish
+  gazebo_pose_pub = nh.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 10);//gazebo_pub
+  Start_Wp_Client = nh.serviceClient<std_srvs::Trigger>("start_wp_nav");  //create start waypoint service  Client for waypoint nav
+    //StartClient = nh.serviceClient<std_srvs::SetBool>("learn_out");  //create start learning service  Client for learning
+    ROS_INFO("ready!");
+  reset_sub = nh.subscribe("reset_pose", 10, &CallBack);//subscribe topic "swiching" from waypoint_nav
+
   Start_Wp_Client = nh.serviceClient<std_srvs::Trigger>("start_wp_nav");  //create start waypoint service  Client for waypoint nav
     //StartClient = nh.serviceClient<std_srvs::SetBool>("learn_out");  //create start learning service  Client for learning
   ROS_INFO("ready!");
